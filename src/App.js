4c4656e095e4100a9c19bb3daf523e188f898618
jaesimin0903 <78476481+jaesimin0903@users.noreply.github.com>
2021-12-19 01:41:44 +0900

    12/10

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
+import React,{ useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  let [noTitle,noTitleChange] = useState(['크리스마스 트리','명동','신세계','쭈꾸미']);
+  let [likeScore,likeScoreChange] = useState(0);
+  
+  let post = '크리스마스';
+
+  const changeTitle = () => {
+    var newArray = [...noTitle];//일반 등호시 주소 참소하기에 값이 변경안됨 [...var] 사용
+    var sub;
+    sub = newArray[0];
+    newArray[0] = newArray[1];
+    newArray[1] = sub;
+    noTitleChange(newArray);
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className = "black-nav">
+          <div>개발 BLOG</div>
+        </div>
+        <button onClick ={changeTitle}>button</button>
+        <div className="list">
+          <h3> { noTitle[0] } <span onClick={ ()=>{likeScoreChange(likeScore + 1)}}>👍</span> { likeScore } </h3>
+          <p>12월 19일 발행</p>
+          <hr/>
+        </div>
+        <div className="list">
+          <h3> { noTitle[1] }</h3>
+          <p>12월 19일 발행</p>
+          <hr/>
+        </div>
+        <div className="list">
+          <h3> { noTitle[2] }</h3>
+          <p>12월 19일 발행</p>
+          <hr/>
+        </div>
+        <div className="list">
+          <h3> { noTitle[3] }</h3>
+          <p>12월 19일 발행</p>
+          <hr/>
+        </div>
     </div>
   );
 }
